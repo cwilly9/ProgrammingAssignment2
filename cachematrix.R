01df7ab12cf21550e1ed44995e13431be06d27ae
unknown <cwilli10@MCTLADV-L0010.tufts.ad.tufts.edu>
2014-05-25 19:18:33 -0400

    Assignment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix Object
+##
+## 2 variables (r_matrix and c_matrix)
+## 4 functions (get,set,getmatrix,setmatrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(r_matrix) {
+  
+  #Initalize the cache
+  c_matrix <- NULL
+  
+  #set the regular matrix and reset the cached version
+  set <- function(y) {
+    r_matrix <<- y
+    c_matrix <<- NULL 
+  }
+  
+  #Return the regular matrix
+  get <- function() {
+       r_matrix
+  }
+  ##Put the input matrix into the cache
+  setmatrix <- function(matrix_tmp) {
+    c_matrix <<- matrix_tmp
+  }
+  
+  ##Get the matrix out of cache and return it to the caller
+  getmatrix <- function() 
+  {
+        c_matrix
+  }
+  
+  ##List of functions in this object
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
-
+##cacheSolve -   Standalone function used to find the inverse of an NxN matrix
+##It expects and utilizes an instance of the cacheMatrix object
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##Check for the existance of a cached version of the inverted matrix
+  tmp_matrix <- x$getmatrix()
+  
+  ##If it exists in cache, return the matrix 
+  if(!is.null(tmp_matrix)) {
+    message("getting cached data")
+    return(tmp_matrix)
+  }
+  ##otherwise get the non-cached, non-inverted matrix and use R's solve function to get the inverse
+  tmp_matrix <- x$get()
+  x_inverse <- solve(tmp_matrix, ...)
+  x$setmatrix(x_inverse)
+  #return the inverse matrix
+  return(x_inverse)
 }
